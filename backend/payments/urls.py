b41120dfce4981093298642188afc0841e8dc9a8
TimsalHassan <TimsalHassan6@gmail.com>
2026-06-03 17:24:39 +0500

    remove all pycache from git tracking

--- backend/payments/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import InitiatePaymentView, PaymentCallbackView
-
-urlpatterns = [
-    path('<str:order_number>/initiate/', InitiatePaymentView.as_view(), name='initiate-payment'),
-    path('callback/success/', PaymentCallbackView.as_view(), kwargs={"result": "success"}, name='payment-success'),
-    path('callback/cancel/', PaymentCallbackView.as_view(), kwargs={"result": "cancel"}, name='payment-cancel'),
-]
--- backend/reviews/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ReviewListView, ReviewCreateView
-
-urlpatterns = [
-    path("", ReviewListView.as_view()),
-    path("create/", ReviewCreateView.as_view()),
-]
