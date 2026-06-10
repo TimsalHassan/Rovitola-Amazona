75fb54a81438e9d25c45a3ed1800da1b72626600
Muhammad Zain <zain683711@gmail.com>
2026-06-10 20:33:28 +0500

    Fix: django-admin path

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
  
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
     path("api/auth/", include("users.urls")),
     path("api/menu/", include("menu.urls")),
     path("api/orders/", include("orders.urls")),
